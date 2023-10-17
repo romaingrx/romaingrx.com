0e3bdf5ac7321f6f0eebe710ff80912206df43b8
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2023-10-17 23:27:16 +0200

    Add pixel background with motion effect

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,46 +1,20 @@
-"use client";
+"use client"
 
-type LiquidGradientProps = {
-    rotation?: number; 
-    color1?: string;
-    color2?: string;
-    seed?: number;
-    baseFrequency?: string;
-    numOctaves?: number;
-    stdDeviation?: string;
-    mode?: string;
-    x?: string;
-    y?: string;
-    width?: string;
-    height?: string;
-};
+import { useEffect, useState } from "react";
+import { RandomPixelBackground } from "@/components/backgrounds/PixelBackground";
 
-function LiquidGradient({
-    rotation = 150,
-    color1 = 'hsl(315, 100%, 72%)',
-    color2 = 'hsl(227, 100%, 50%)',
-    seed = 2,
-    baseFrequency = '0.005 0.003',
-    numOctaves = 2,
-    stdDeviation = '20 0',
-    mode = 'color-dodge',
-    x = '-20%',
-    y = '-20%',
-    width = '100%',
-    height = '100%',
-}: LiquidGradientProps) : React.ReactElement {
+export default function Test() {
+    const [visible, setVisible] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setVisible(false);
+        }, 1000);
+    }, []);
+
     return (<>
-        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 700 700" width={width} height={height}><defs><linearGradient gradientTransform={`rotate(${rotation}, 0.5, 0.5)`} x1="50%" y1="0%" x2="50%" y2="100%" id="ffflux-gradient"><stop stop-color={color1} stop-opacity="1" offset="0%"></stop><stop stop-color={color2} stop-opacity="1" offset="100%"></stop></linearGradient><filter id="ffflux-filter" x={x} y={y} width={width} height={height} filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-            <feTurbulence type="fractalNoise" baseFrequency={baseFrequency} numOctaves={numOctaves} seed={seed} stitchTiles="stitch" x={x} y={y} width={width} height={height} result="turbulence"></feTurbulence>
-            <feGaussianBlur stdDeviation={stdDeviation} x={x} y={y} width={width} height={height} in="turbulence" edgeMode="duplicate" result="blur"></feGaussianBlur>
-            <feBlend mode={mode} x={x} y={y} width={width} height={height} in="SourceGraphic" in2="blur" result="blend"></feBlend>
-
-        </filter></defs><rect width={width} height={height} fill="url(#ffflux-gradient)" filter="url(#ffflux-filter)"></rect></svg>
-    </>);
-}
-
-export default function Page() {
-    return (<>
-        <LiquidGradient />
-    </>);
+        <div className="h-screen w-screen fixed top-0 left-0">
+            <RandomPixelBackground visible={visible} />
+        </div>
+    </>)
 }
---
+++ components/backgrounds/PixelBackground.tsx
@@ -0,0 +1,92 @@
+"use client"
+
+import { TargetAndTransition, motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import clsx from "clsx";
+
+type PixelBackgroundProps = {
+    visible: boolean;
+    columns?: number;
+    className?: string;
+    backgroundColor?: string;
+}
+
+type CorePixelBackgroundProps = PixelBackgroundProps & {
+    variants: {
+        hidden: ({ row, column, rows, columns }: { row: number, column: number, rows: number, columns: number }) => TargetAndTransition;
+        visible: ({ row, column, rows, columns }: { row: number, column: number, rows: number, columns: number }) => TargetAndTransition;
+    };
+}
+
+function CorePixelBackground({
+    visible,
+    className,
+    columns = 20,
+    variants = {
+        hidden: () => ({ opacity: 0 }),
+        visible: () => ({ opacity: 1 }),
+    },
+    backgroundColor = "var(--color-bob)",
+}: CorePixelBackgroundProps): JSX.Element {
+
+    const { innerWidth, innerHeight } = window;
+    const pixelSize = innerWidth / columns;
+    const rows = Math.ceil(innerHeight / pixelSize);
+
+    return (<>
+        <div className={clsx(className, "flex h-full")} onClick={() => { }}>
+            {Array.from(Array(columns).keys()).map((_, column) => {
+                return (<div
+                    className="h-full"
+                    style={{
+                        width: `${100 / columns}vw`
+                    }}
+                >
+                    {Array.from(Array(rows).keys()).map((_, row) => {
+                        return (<motion.div
+                            className="h-full"
+                            variants={variants}
+                            initial={'hidden'}
+                            animate={visible ? 'visible' : 'hidden'}
+                            custom={{ row, column, rows, columns }}
+                            style={{
+                                height: `${pixelSize}px`,
+                                width: `${100 / columns}vw`,
+                                backgroundColor: backgroundColor
+                            }}
+                        />)
+                    })}
+                </div>)
+            })}
+        </div>
+    </>)
+}
+
+type RandomPixelBackgroundProps = PixelBackgroundProps & {
+    delay?: number;
+    duration?: number;
+}
+
+const RandomPixelBackground = ({ visible, className, delay = 0.5, duration = 0.1 }: RandomPixelBackgroundProps): JSX.Element => {
+    return (<CorePixelBackground
+        visible={visible}
+        className={className}
+        columns={Math.floor(Math.random() * 10) + 1}
+        variants={{
+            hidden: () => ({
+                opacity: 0,
+                transition: {
+                    duration: duration,
+                    delay: delay * Math.random(),
+                },
+            }),
+            visible: () => ({
+                opacity: 1,
+            }),
+        }}
+    />)
+}
+
+export default CorePixelBackground;
+export type { CorePixelBackgroundProps, PixelBackgroundProps, RandomPixelBackgroundProps };
+export { RandomPixelBackground };
