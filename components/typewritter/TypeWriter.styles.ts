2b1d2cf77b9624774a93be7c3964838926984c7e
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2023-11-07 20:42:47 +0100

    Fix styled type writer

--- components/typewritter/TypeWriter.styles.ts
+++ components/typewritter/TypeWriter.styles.ts
@@ -1,8 +1,4 @@
 import TypeWriter from 'typewriter-effect';
 import { styled } from '@/design';
 
-export const StyledTypeWriter = styled(TypeWriter, {
-    "&& .Typewriter__wrapper": {
-        color: 'var(--romaingrx-colors-brand)',
-    }
-});
+export const StyledTypeWriter = styled(TypeWriter, {});
