dacb9b568d1dbc7b8d8b620a86e97c901fd18d80
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2024-01-26 08:32:08 +0100

    fix font path

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,14 +1,9 @@
 import fs from 'fs';
 import path from 'path';
 
+
 export async function getFontasArrayBuffer(fontName: string) {
-  const fontPath = path.join(
-    process.env.PWD!,
-    'assets',
-    'fonts',
-    `${fontName}`,
-  );
-  console.log(fontPath);
+  const fontPath = path.join(process.cwd(), `./assets/fonts/${fontName}`);
   const arrayBuffer = fs.readFileSync(fontPath);
   return arrayBuffer;
 }
