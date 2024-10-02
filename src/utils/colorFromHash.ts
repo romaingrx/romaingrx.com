69bdbcbeaa04f99b673f1dcd1fc6bef1c85db0be
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2024-10-03 00:43:53 +0200

    Basic astro blog

---
+++ src/utils/colorFromHash.ts
@@ -0,0 +1,14 @@
+function colorFromHash(hash: string): [string, string] {
+  // Simple hash function
+  const hashValue = hash.split("").reduce((acc, char) => {
+    return char.charCodeAt(0) + ((acc << 5) - acc);
+  }, 0);
+
+  // Generate two colors from the hash
+  const color1 = `hsl(${hashValue % 360}, 70%, 20%)`;
+  const color2 = `hsl(${(hashValue * 2) % 360}, 70%, 40%)`;
+
+  return [color1, color2];
+}
+
+export default colorFromHash;
