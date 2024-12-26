d2bac32052b6d8849260cc1659097da6909ab6e7
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2024-12-26 16:58:51 +0100

    Add category and tag badge components for blog posts
    - Introduced `CategoryBadge` and `TagBadge` components to enhance the presentation of categories and tags in blog posts.
    - Updated the `Footer`, `BlogPost`, and various blog-related pages to utilize the new badge components for improved layout and styling.
    - Enhanced the `Badge` component with a new 'active' variant for better visual feedback.
    - Created new pages for category and tag views, allowing users to explore posts by category or tag.

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -14,6 +14,7 @@ const badgeVariants = cva(
         destructive:
           'border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80',
         outline: 'text-foreground',
+        active: 'border-transparent bg-violet-500/10 text-violet-500 shadow hover:bg-violet-500/20',
       },
     },
     defaultVariants: {
